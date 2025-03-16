dce3c055cf3416d717856c219e6036af3a9a0c18
Andrii Zadorozhnii <zador746@gmail.com>
2025-03-16 13:13:26 +0200

    #9. Шаблоны, начало. Функции render() и render to string() | Уроки по Django 4

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 
 def index(request):
-    return HttpResponse('hello my dear friend')
+    return render(request, 'women/index.html')
 
+def about(request):
+    return render(request, 'women/about.html')
 
 def categories(request, cat_id):
     return HttpResponse(f'<h1>Category</h1><p>id: {cat_id}</p>')
