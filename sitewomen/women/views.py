7d9e3fd96c7ada8229f8c0fcc52041c18dac4855
Andrii Zadorozhnii <zador746@gmail.com>
2025-03-16 13:48:01 +0200

    #10. Передача данных (переменных) в шаблоны | Уроки по Django 4

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -3,12 +3,35 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 
+menu = ["About", 'Add post', 'Contact', 'Login']
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
 
 def index(request):
-    return render(request, 'women/index.html')
+
+    data = {
+        "title": "Main page",
+        "menu": menu,
+        "float": 28.56,
+        "lst": [1, 2, "three", True],
+        "set": {1,2,3,4,5},
+        "dict": {"key_1": "value_1", "key_2": "value_2"},
+        "obj": MyClass(10,20),
+    }
+    return render(request, 'women/index.html', data)
+
 
 def about(request):
-    return render(request, 'women/about.html')
+
+    data = {"title": "About"}
+    return render(request, 'women/about.html', data)
+
+
+
 
 def categories(request, cat_id):
     return HttpResponse(f'<h1>Category</h1><p>id: {cat_id}</p>')
@@ -31,3 +54,4 @@ def archive(request, year):
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Page not found</h1>')
+
